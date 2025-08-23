423ffe7ff4e47e56b41e431f75c3b6988c7cc0a8
NguyenQuocCuong24 <quoccuong2493@gmail.com>
2025-08-23 14:34:00 +0700

    layout

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="mx-auto max-w-[1550px] px-4 py-16 grid grid-rows-[auto_1fr_auto] min-h-screen">
+      <h1 className="text-4xl font-bold mb-8">Welcome to My Next.js App</h1>
+    </div>
+  );
+}
